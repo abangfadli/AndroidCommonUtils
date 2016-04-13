938d2fc8e4ce4f2559d19725f59e0f680c9d27cb
Ahmad Fadli Basyari <ahmad.fadli.b@gmail.com>
2016-04-14 01:57:58 +0700

    Initial Commit

---
+++ CommonUtils-core/src/main/java/com/abangfadli/simplemvp/StringUtil.java
@@ -0,0 +1,10 @@
+package com.abangfadli.simplemvp;
+
+/**
+ * Created by ahmadfadli on 4/14/16.
+ */
+public class StringUtil {
+    public static boolean isNullOrEmpty(String str) {
+        return str == null || str.isEmpty();
+    }
+}
---
+++ CommonUtils-core/src/main/java/com/abangfadli/simplemvp/ViewUtil.java
@@ -0,0 +1,22 @@
+package com.abangfadli.simplemvp;
+
+import android.widget.TextView;
+
+/**
+ * Created by ahmadfadli on 4/14/16.
+ */
+public class ViewUtil {
+    public static void setTextIfNotNull(TextView tv, String text) {
+        if(!StringUtil.isNullOrEmpty(text)) {
+            tv.setText(text);
+        }
+    }
+
+    public static void setTextIfNotNull(TextView tv, String text, String defaultIfNull) {
+        if(!StringUtil.isNullOrEmpty(text)) {
+            tv.setText(text);
+        } else {
+            tv.setText(defaultIfNull);
+        }
+    }
+}
